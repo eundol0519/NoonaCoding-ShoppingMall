f8c67f072468296caedfd105fea767c7ed6c8917
으니 <72530436+eundol0519@users.noreply.github.com>
2024-04-13 16:14:32 +0900

    📝 : Setting React-Router-Dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import App from './App';
+import List from './component/List';
+import Detail from './component/Detail';
+import Login from './component/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+/*
+  [유저 스토리]
+  - 로그인, 전체, 상세 페이지 필요
+  - 전체 페이지는 전체 상품 볼 수 있음
+  - 로그인 버튼을 누르면 로그인 화면으로
+  - 상세 페이지는 로그인이 되어 있으면 상세로, 안되어 있으면 로그인으로
+  - 로그아웃 버튼 누르면 alert 띄우면서 로그인 페이지로 이동
+  - 로그인을 하면 로그아웃 버튼, 로그아웃이면 로그인 버튼 노출
+  - 상품 검색 기능 필요
+*/
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<List />} />
+        <Route path="/products/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
